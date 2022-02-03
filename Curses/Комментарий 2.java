2a287ca42b01f83edd3e532e2bc01fc51ed121e4
SerJas2042 <91256838+SerJas2042@users.noreply.github.com>
2022-02-03 12:43:38 +0300

    Create Комментарий 2.java

---
+++ Curses/Комментарий 2.java
@@ -0,0 +1,7 @@
+class MyProgram{
+    public static void main(String[] args){
+        System.out.print("Я прекрасно понимаю разницу ");
+        System.out.println("между System.out.println()");
+        System.out.print("и System.out.print(). А ты?");
+    }
+}
